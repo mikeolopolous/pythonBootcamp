2339fcbc31df55910cc2f2ce69595f3954fb98e6
Miguel Lopez <ing.miguellv@gmail.com>
2024-01-23 11:41:11 -0600

    instancia de Food y método refresh

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 
@@ -10,17 +11,22 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up, key='Up')
 screen.onkey(snake.down, key='Down')
-screen.onkey(snake.left, key='Left')
-screen.onkey(snake.right, key='Right')
+screen.onkey(snake.left_move, key='Left')
+screen.onkey(snake.right_move, key='Right')
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.5)
+    time.sleep(0.2)
 
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
