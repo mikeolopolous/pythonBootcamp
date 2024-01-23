18b74068cc1567d9c59a7635dbda126c198d145e
Miguel Lopez <ing.miguellv@gmail.com>
2024-01-22 19:08:26 -0600

    agrego control de la serpiente

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,9 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
 
-
-STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,23 +9,18 @@ screen.bgcolor('black')
 screen.title("Snake Game")
 screen.tracer(0)
 
-for position in STARTING_POSITIONS:
-    new_segment = Turtle(shape="square")
-    new_segment.color('white')
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, key='Up')
+screen.onkey(snake.down, key='Down')
+screen.onkey(snake.left, key='Left')
+screen.onkey(snake.right, key='Right')
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.5)
 
-    for segment_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[segment_num - 1].xcor()
-        new_y = segments[segment_num - 1].ycor()
-        segments[segment_num].goto(new_x, new_y)
-
-    segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
