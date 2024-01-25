94a7e5f0be7fa21649d36632f8b39a77d5827004
Miguel Lopez <ing.miguellv@gmail.com>
2024-01-24 19:29:54 -0600

    detecto cuando la bola cruza los limites de ambos paddles

--- PongGame/main.py
+++ PongGame/main.py
@@ -28,6 +28,17 @@ while game_is_on:
     ball.move()
 
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # L paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # R paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
