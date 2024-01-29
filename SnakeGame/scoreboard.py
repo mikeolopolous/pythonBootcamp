51f240394605f0025a8efeef9f8477014becd1cd
Miguel Lopez <ing.miguellv@gmail.com>
2024-01-29 10:38:22 -0600

    Agrego funcionalidad para high score persistente

--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -10,13 +10,22 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.goto(x=0, y=270)
         self.color("medium purple")
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+
+        self.score = 0
+        self.update_scoreboard()
 
     def game_over(self):
         self.goto(0, 0)
@@ -24,5 +33,4 @@ class Scoreboard(Turtle):
 
     def new_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
