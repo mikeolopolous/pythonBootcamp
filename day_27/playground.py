d0b29f7d0ffd9cc77b3afbc18078c64ddbb9e336
Miguel Lopez <ing.miguellv@gmail.com>
2024-02-06 18:48:37 -0600

    Primer uso de **kwargs

--- day_27/playground.py
+++ day_27/playground.py
@@ -1,3 +1,4 @@
+# Uso de *args
 def add(*args):
     suma = 0
     for num in args:
@@ -7,3 +8,16 @@ def add(*args):
 
 
 print(add(2, 5, 8))
+
+
+# Uso de **kwargs
+def calculate(n, **kwargs):
+    print(kwargs)
+
+    suma = n + kwargs["add"]
+    multiplicacion = n * kwargs["multiply"]
+
+    return suma, multiplicacion
+
+
+print(calculate(2, add=3, multiply=5))
