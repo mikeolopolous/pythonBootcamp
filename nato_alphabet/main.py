452fd1a37abfcc76faf1e7f83f93878b83e8a93e
Miguel Lopez <ing.miguellv@gmail.com>
2024-02-19 11:00:33 -0600

    agrego exceptions al ingresar texto

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -1,10 +1,20 @@
 import pandas as pd
 
 
-student_data_frame = pd.read_csv("./nato_alphabet/nato_phonetic_alphabet.csv")
+student_data_frame = pd.read_csv("nato_phonetic_alphabet.csv")
 
-nato_alphabet = {row.letter:row.code for (index, row) in student_data_frame.iterrows()}
+nato_alphabet = {row.letter: row.code for (index, row) in student_data_frame.iterrows()}
 
-word = input("Enter a word: ").upper()
-result = [nato_alphabet[letter] for letter in word ]
-print(result)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        result = [nato_alphabet[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(result)
+
+
+generate_phonetic()
