6bf095b638c0c69f461393bc0bd97eb61c7c14fa
Miguel Lopez <ing.miguellv@gmail.com>
2024-02-21 16:04:47 -0600

    creo GUI

--- flash_card/main.py
+++ flash_card/main.py
@@ -1,2 +1,26 @@
+from tkinter import *
+
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 258, image=front_img)
+title_text = canvas.create_text(400, 150, text="Title", fill="black", font=("Arial", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="Word", fill="black", font=("Arial", 60, "bold"))
+canvas.grid(column=0, row=0, columnspan=2)
+
+wrong_img = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_img, highlightbackground=BACKGROUND_COLOR)
+wrong_button.grid(column=0, row=1)
+
+right_img = PhotoImage(file="images/right.png")
+right_button = Button(image=right_img, highlightbackground=BACKGROUND_COLOR)
+right_button.grid(column=1, row=1)
+
+window.mainloop()
